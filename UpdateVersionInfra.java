65224456c0ab93431cb5a78b2ea1ae51c8dea88d
SamanOffice <75738542+SamanOffice@users.noreply.github.com>
2021-01-11 12:24:22 +0330

    Update UpdateVersionInfra.java

--- UpdateVersionInfra.java
+++ UpdateVersionInfra.java
@@ -1,60 +1,4 @@
-package com.cinnadco.distribution.Business.Infra;
 
-import android.app.Activity;
-import android.app.Dialog;
-import android.app.DownloadManager;
-import android.app.ProgressDialog;
-import android.content.BroadcastReceiver;
-import android.content.Context;
-import android.content.DialogInterface;
-import android.content.Intent;
-import android.content.IntentFilter;
-import android.content.pm.ApplicationInfo;
-import android.content.pm.PackageInfo;
-import android.content.pm.PackageManager;
-import android.net.Uri;
-import android.os.AsyncTask;
-import android.os.Build;
-import android.os.Environment;
-import android.provider.MediaStore;
-import android.util.Log;
-import android.widget.Toast;
-
-import androidx.core.content.FileProvider;
-
-import com.cinnadco.distribution.Business.Models.LatestVersionModel;
-import com.cinnadco.distribution.Business.Models.OperationResult;
-import com.cinnadco.distribution.Business.Models.UpdateVersionModel;
-import com.cinnadco.distribution.Business.Models.UserInformations;
-import com.cinnadco.distribution.DownloadFileFromURL;
-import com.cinnadco.distribution.Infrastructure.Utility;
-import com.cinnadco.distribution.BuildConfig;
-import com.cinnadco.distribution.Helper.DialogMessage;
-import com.cinnadco.distribution.Helper.ProgressbarHelper;
-import com.cinnadco.distribution.R;
-import com.cinnadco.distribution.UI.Activities.LoginActivity;
-import com.cinnadco.distribution.UI.CustomeView.Dialog.AlertDialog;
-import com.cinnadco.distribution.loginfo.LogInfoActivity;
-import com.cinnadco.distribution.loginfo.model.LogInfoModel;
-import com.cinnadco.distribution.loginfo.room.NoteDatabase;
-import com.cinnadco.distribution.loginfo.room.dao.RoomDAO;
-
-import java.io.BufferedInputStream;
-import java.io.File;
-import java.io.FileInputStream;
-import java.io.FileNotFoundException;
-import java.io.FileOutputStream;
-import java.io.InputStream;
-import java.io.OutputStream;
-import java.net.URL;
-import java.net.URLConnection;
-import java.util.ArrayList;
-import java.util.Calendar;
-import java.util.List;
-
-import retrofit2.Call;
-import retrofit2.Callback;
-import retrofit2.Response;
 
 public class UpdateVersionInfra {
 
@@ -76,108 +20,14 @@ public class UpdateVersionInfra {
 
      private static Context mContext;
     public static void CheckLatestVersion(final Activity context, final Boolean isAutoCheck){
-        mContext=context;
-        if(!isAutoCheck) ProgressbarHelper.Show(context);
-        List<PackageInfo> packageList = context.getPackageManager().getInstalledPackages(0);
-        List<String> appNames = new ArrayList<>();
-        for (PackageInfo pc :packageList) {
-            if (  (pc.applicationInfo.flags & ApplicationInfo.FLAG_SYSTEM) == 0) {
-                appNames.add(pc.packageName);
-            }
-        }
-        PackageInfo pinfo = null;
-        try {
-            pinfo = context.getPackageManager().getPackageInfo(context.getPackageName(), 0);
-
-
-        } catch (PackageManager.NameNotFoundException e) {
-            e.printStackTrace();
-
-            getLogInfo(LogInfoActivity.filterName[3],"خطای NameNotFoundException   در بروزرسانی کلی",e.getMessage());
-        }
-
-        final UpdateVersionModel model= new UpdateVersionModel(appNames,pinfo.versionName,pinfo.versionCode);
-
-        Log.e(TAG, "CheckLatestVersion: "+pinfo.versionName+"    "+pinfo.versionCode);
+    
 
         Callback<OperationResult<LatestVersionModel>> callBack = new Callback<OperationResult<LatestVersionModel>>() {
             @Override
             public void onResponse(Call<OperationResult<LatestVersionModel>> call, Response<OperationResult<LatestVersionModel>> response) {
                 ProgressbarHelper.Dismiss();
 
-                Log.e(TAG, "onResponse: "+response.toString());
-
-                String sep[]=response.toString().split(",");
-                String tempLogHandle=sep[0]+","+sep[1]+","+sep[2];
-                getLogInfo(LogInfoActivity.filterName[3],"کنترل درخواست  در بروزرسانی کلی",tempLogHandle);
-
-                try {
-
-                    final OperationResult<LatestVersionModel> data = response.body();
-
-                    Log.e(TAG, "Exception: "+data.Exception );
-
-
-                    if (!response.isSuccessful() || data == null) {
-                        getLogInfo(LogInfoActivity.filterName[3],"خطای  data == null  در بروزرسانی کلی","!response.isSuccessful() || data == null");
-                        Log.e(TAG, "!response.isSuccessful() || data == null ");
-                        return;
-                    }
-                    if (model.VersionName.equals(data.Item.VersionName)) {
-                        if(!isAutoCheck){
-                            DialogMessage.ShowDialog(context,"نرم افزار به روز می باشد");
-                            return;
-                        }
-                        return;
-                    }
-
-
-                    AlertDialog dialog = new AlertDialog(context);
-                    String message = "نسخه جدید نرم افزار آماده بروز رسانی می باشد.";
-                    if(data.Item.IsForce){
-                        dialog.setCancelable(false);
-                        message += " لطفا با ورژن جدید وارد شوید ";
-                    }else{
-                        dialog.setCancelButton();
-                        message += "آیا مایل به بروز رسانی می باشید؟";
-                    }
-                    dialog.setMessage(message);
-
-                    dialog.setAcceptButton(new AlertDialog.OnAcceptInterface() {
-
-                        @Override
-                        public void accept() {
-                        /*ProgressbarHelper.Show(context);
-                        Download(data.Item.UpdatePath, context);*/
-
-                            Download(file_url,mContext);
-                       //   //  ShowSuccessAlert("تست");
                          // new DownloadFileFromURL().execute(file_url);
-
-//
-//                            try {
-//                                Uri uri = Uri.parse("googlechrome://navigate?url=" + data.Item.UpdatePath);
-//                                Intent i = new Intent(Intent.ACTION_VIEW, uri);
-//                                if (i.resolveActivity(context.getPackageManager()) == null) {
-//                                    i.setData(Uri.parse(data.Item.UpdatePath));
-//                                }
-//                                context.startActivity(i);
-//                            }
-//                            catch(Exception ex){
-//                                Intent browserIntent = new Intent(Intent.ACTION_VIEW, Uri.parse(data.Item.UpdatePath));
-//                                context.startActivity(browserIntent);
-//                            }
-
-                        }
-                    });
-                    dialog.show();
-
-
-                    Log.e(TAG, "UpdatePath  Imp: "+data.Item.UpdatePath );
-                    Log.e(TAG, "VersionName: "+data.Item.VersionName );
-                    Log.e(TAG, "IsForce: "+data.Item.IsForce );
-                    Log.e(TAG, "AppMessage: "+data.Item.AppMessage );
-
 
 
                 }catch(Exception ex){
@@ -270,19 +120,6 @@ public class UpdateVersionInfra {
 
 
 
-    private static void getLogInfo(String title, String message, String Exception) {
-        NoteDatabase appDatabase = NoteDatabase.getInstance(mContext);
-        RoomDAO roomDAO = appDatabase.getNoteDao();
-        LogInfoModel model = new LogInfoModel();
-        model.setTitle(title);
-        model.setMessage(message);
-        model.setException(Exception);
-        Calendar calendar = Calendar.getInstance();
-        model.setActionDate(calendar.getTime());
-        model.setUserId(UserInformations.getUserId());
-        roomDAO.Insert(model);
-
-    }
 
 
     private static   Dialog ShowDialog(){
